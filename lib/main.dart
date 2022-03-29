72e934bc05325e4455404691cfa6eb78e50a0202
AJ-Kulundu <jameskulundu@gmail.com>
2022-03-29 12:45:51 +0300

    Flutter layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,28 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                height: double.infinity,
+                width: 100.0,
+              ),
+              Container(
+                color: Colors.yellow,
+                height: 100.0,
+                width: 100.0,
+              ),
+              Container(
+                color: Colors.blue,
+                height: double.infinity,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
